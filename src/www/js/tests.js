9cfa5b890b41e549947eafe4d3d2446c35abec12
George Hamilton <george.hamilton@ed.ac.uk>
2014-05-07 10:49:18 +0100

    Issue #31 remove dummy test

--- src/www/js/tests.js
+++ src/www/js/tests.js
@@ -1,9 +1,8 @@
 "use strict";
 define(['./map-search'], function(search) {
     var run = function() {
-        test('Map Search: do something.', function() {
-            equal(2, 2, 'The return should be 2.');
-        });
+        module("Map Search");
     };
+
     return {run: run}
 });
